48da78a843aa679ebbd9a3cefb4d17fe76ddb400
Nicolas Berbiche <nic.berbiche@gmail.com>
2020-06-06 17:39:45 -0400

    Add pre-commit-hook with nixpkgs-fmt

--- shell.nix
+++ shell.nix
@@ -1,9 +1,18 @@
 let
   sources = import ./nix/sources.nix;
-  pkgs = import sources.nixpkgs { };
+  pkgs = import sources.nixpkgs {};
+  src = (import sources.gitignore {}).gitignoreSource ./.;
+
+  pre-commit-hook = (import sources.pre-commit-hooks).run {
+    inherit src;
+    hooks = {
+      nixpkgs-fmt.enable = true;
+      shellCheck.enable = false; # Not working
+    };
+    excludes = [ "nix/sources.nix$" ];
+  };
 in
 pkgs.mkShell {
-  buildInputs = [
-    pkgs.nixfmt
-  ];
+  inherit (pre-commit-hook) shellHook;
+  buildInputs = [ pkgs.nixops ];
 }
